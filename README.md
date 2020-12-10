4e4df35be61da267f71163632fd8038bb52af2c5
Robert Rothenberg <rrwo@cpan.org>
2020-12-10 12:36:05 +0000

    Bump VERSION to v0.3.0

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Text::Minify::XS - Simple text minification
 
 # VERSION
 
-version v0.2.3
+version v0.3.0
 
 # SYNOPSIS
 
--- lib/Text/Minify/XS.pm
+++ lib/Text/Minify/XS.pm
@@ -12,7 +12,7 @@ require XSLoader;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(minify);
 
-our $VERSION = 'v0.2.4';
+our $VERSION = 'v0.3.0';
 
 XSLoader::load( "Text::Minify::XS", $VERSION );
 
