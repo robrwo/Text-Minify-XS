7aa7df2372921b1d770b17a11ce9ae8fcfe41011
Robert Rothenberg <rrwo@cpan.org>
2021-09-18 13:54:33 +0100

    Commit Changes and bump $VERSION

--- lib/Text/Minify/XS.pm
+++ lib/Text/Minify/XS.pm
@@ -12,7 +12,7 @@ require XSLoader;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(minify);
 
-our $VERSION = 'v0.4.10';
+our $VERSION = 'v0.4.11';
 
 XSLoader::load( "Text::Minify::XS", $VERSION );
 
